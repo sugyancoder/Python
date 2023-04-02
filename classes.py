d36abee4a11a0e564967d6ba89d5376b277a5837
sugyancoder <129498460+sugyancoder@users.noreply.github.com>
2023-04-02 12:49:05 +0200

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,12 @@
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+        def add_passenger(self, name):
+            self.passengers.append(name)
+
+            def open_seats()
+                
+
+                flight = Flight(3)
