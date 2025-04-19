27ff6e665c06da1b59d5bfa8463ac379f071e524
sugyancoder <129498460+sugyancoder@users.noreply.github.com>
2025-04-19 15:21:45 +0200

    Add files via upload

---
+++ MathInPython.py
@@ -0,0 +1,7 @@
+import math
+a=float(input("Enter Side A:"))
+b=float(input("Enter Side B:"))
+
+c= math.sqrt(a**2 + b**2)
+
+print("Hypotenuse {Side C} :", float(c))
---
+++ variables.py
@@ -0,0 +1,29 @@
+#Variable = A container for a value (string, integer, float, boolean)
+#A variable behaves as if it was the value it contains
+
+#Strings
+first_name = "Sugyan Lama"
+food = "pizza"
+email = "sugyancoder@gmail.com"
+
+#Integers
+age = 16
+quantity = 3
+num_of_students = 20
+
+#Float
+price = 10.99
+distance = 5.5
+
+#Boolean
+is_student = True
+for_sale = False
+is_online = True
+
+print(f"Hello {first_name}")
+print(f"You are {age} years old")
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT student")
